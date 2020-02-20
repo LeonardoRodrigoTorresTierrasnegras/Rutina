90b62fb5ff96a64222bde1167120e946b24769fa
LeonardoRodrigoTorresTierrasnegras <61294311+LeonardoRodrigoTorresTierrasnegras@users.noreply.github.com>
2020-02-20 16:03:48 -0600

    Add files via upload

---
+++ ProgramaDeLaSemana.py
@@ -0,0 +1,236 @@
+
+print ("Buenos dias \n")
+print ("¿Desea examinar la semana del estudiante?: ")
+print(" SI. Para aceptar")
+print(" NO. Para denegar")
+
+y=input("Seleccione una opcion por favor:  ")
+
+if (y=="SI"):
+
+
+ print("\n Indica el dia a ver")
+ print("  L. Para el dia lunes")
+ print("  M. Para el dia martes")
+ print(" MI. Para el dia miercoles")
+ print("  J. Para el dia jueves")
+ print("  V. Para el dia veirnes")
+
+ x=input("¿Que dia quieres ver?: ")
+
+ if (x=="L"):
+
+  print("\nSelecciona la hora a ver: ")
+  print(" A. Para las 7 A.M")
+  print(" B. Para las 8 A.M")
+  print(" V. Para las 9 A.M")
+  print(" D. Para las 10 A.M")
+  print(" E. Para las 11 A.M")
+  print(" F. Para las 12 P.M")
+  print(" G. Para las 13 P.M")
+  print(" H. Para las 14 P.M")
+  print(" I. Para las 15 AP.M")
+
+  n= input("¿Que valor quieres?: ")
+
+
+  if (n=="A"):
+    print ("Clase de frances")
+
+  if (n=="B"):
+    print ("Clase de microcontroladores")
+
+  if (n=="C"):
+    print ("Clase de competencias directivas")
+
+  if (n=="D"):
+    print ("Clase de programacion avanzada")
+
+  if (n=="E"):
+    print ("Comer una torta")
+
+  if (n=="F"):
+    print ("Clase de dinamica de sistemas")
+
+  if (n=="G"):
+    print ("Clase de instrumentacion")
+ 
+  if (n=="H"):
+    print ("Comer un dulce")
+
+  if (n=="I"):
+    print ("Salir del tec ")
+
+ if (x=="M"):
+
+  print("\nSelecciona la hora a ver: ")
+  print(" A. Para las 7 A.M")
+  print(" B. Para las 8 A.M")
+  print(" V. Para las 9 A.M")
+  print(" D. Para las 10 A.M")
+  print(" E. Para las 11 A.M")
+  print(" F. Para las 12 P.M")
+  print(" G. Para las 13 P.M")
+  print(" H. Para las 14 P.M")
+  print(" I. Para las 15 AP.M")
+
+  n= input("¿Que valor quieres?: ")
+
+  if (n=="A"):
+    print ("Clase de frances")
+
+  if (n=="B"):
+    print ("Clase de microcontroladores")
+
+  if (n=="C"):
+    print ("Clase de competencias directivas")
+
+  if (n=="D"):
+    print ("Clase de programacion avanzada")
+
+  if (n=="E"):
+    print ("Comer una torta")
+
+  if (n=="F"):
+    print ("Clase de dinamica de sistemas")
+
+  if (n=="G"):
+    print ("Clase de instrumentacion")
+ 
+  if (n=="H"):
+    print ("Comer un dulce")
+
+  if (n=="I"):
+    print ("Salir del tec ")
+
+ if (x=="MI"):
+
+  print("\nSelecciona la hora a ver: ")
+  print(" A. Para las 7 A.M")
+  print(" B. Para las 8 A.M")
+  print(" V. Para las 9 A.M")
+  print(" D. Para las 10 A.M")
+  print(" E. Para las 11 A.M")
+  print(" F. Para las 12 P.M")
+  print(" G. Para las 13 P.M")
+  print(" H. Para las 14 P.M")
+  print(" I. Para las 15 AP.M")
+
+  n= input("¿Que valor quieres?: ")
+
+  if (n=="A"):
+    print ("Clase de frances")
+
+  if (n=="B"):
+    print ("Clase de microcontroladores")
+
+  if (n=="C"):
+    print ("Clase de competencias directivas")
+
+  if (n=="D"):
+    print ("Clase de programacion avanzada")
+
+  if (n=="E"):
+    print ("Comer una torta")
+
+  if (n=="F"):
+    print ("Clase de dinamica de sistemas")
+
+  if (n=="G"):
+    print ("Clase de instrumentacion")
+ 
+  if (n=="H"):
+    print ("Comer un dulce")
+
+  if (n=="I"):
+    print ("Salir del tec ")
+
+ if (x=="J"):
+
+  print("\nSelecciona la hora a ver: ")
+  print(" A. Para las 7 A.M")
+  print(" B. Para las 8 A.M")
+  print(" V. Para las 9 A.M")
+  print(" D. Para las 10 A.M")
+  print(" E. Para las 11 A.M")
+  print(" F. Para las 12 P.M")
+  print(" G. Para las 13 P.M")
+  print(" H. Para las 14 P.M")
+  print(" I. Para las 15 AP.M")
+
+  n= input("¿Que valor quieres?: ")
+
+  if (n=="A"):
+    print ("Clase de frances")
+
+  if (n=="B"):
+    print ("Clase de microcontroladores")
+
+  if (n=="C"):
+    print ("Clase de competencias directivas")
+
+  if (n=="D"):
+    print ("Clase de programacion avanzada")
+
+  if (n=="E"):
+    print ("Comer una torta")
+
+  if (n=="F"):
+    print ("Clase de dinamica de sistemas")
+
+  if (n=="G"):
+    print ("Clase de instrumentacion")
+ 
+  if (n=="H"):
+    print ("Comer un dulce")
+
+  if (n=="I"):
+    print ("Salir del tec ")
+
+ if (x=="V"):
+
+  print("\nSelecciona la hora a ver: ")
+  print(" A. Para las 7 A.M")
+  print(" B. Para las 8 A.M")
+  print(" V. Para las 9 A.M")
+  print(" D. Para las 10 A.M")
+  print(" E. Para las 11 A.M")
+  print(" F. Para las 12 P.M")
+  print(" G. Para las 13 P.M")
+  print(" H. Para las 14 P.M")
+  print(" I. Para las 15 AP.M")
+
+  n= input("¿Que valor quieres?: ")
+
+  if (n=="A"):
+    print ("Clase de frances")
+
+  if (n=="B"):
+    print ("Clase de microcontroladores")
+
+  if (n=="C"):
+    print ("Clase de competencias directivas")
+
+  if (n=="D"):
+    print ("Clase de programacion avanzada")
+
+  if (n=="E"):
+    print ("Comer una torta")
+
+  if (n=="F"):
+    print ("Clase de dinamica de sistemas")
+
+  if (n=="G"):
+    print ("Clase de instrumentacion")
+ 
+  if (n=="H"):
+    print ("Comer un dulce")
+
+  if (n=="I"):
+    print ("Salir del tec ")
+
+if (y=="no"):
+    print("Adios popo")
+
+
+
